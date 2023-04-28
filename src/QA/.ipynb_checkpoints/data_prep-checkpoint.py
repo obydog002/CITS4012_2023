fb9880282d5678ce3c7cacbfbad85a1dce86ab5a
obydog002 <mchatfield@posteo.net>
2023-04-28 15:44:13 +0800

    add data, package

---
+++ src/QA/.ipynb_checkpoints/data_prep-checkpoint.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def parse_tsv(path):
+    df = pd.dataframe(path)
+    print(df.head())
+    
+    # pandas dataframe
+    pass
+
+def tokenizer(words):
+    pass
+
+def encode_answer_types(data):
+    pass
+
