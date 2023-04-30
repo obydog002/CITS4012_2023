b164ff4bea21174c4d8b2b7f28d85f21c34b6ff8
ndeclarke <ndeclarke@gmail.com>
2023-04-30 06:21:07 +0000

    added pos tags, but needs conversion to index

--- src/QA/word_embed.py
+++ src/QA/word_embed.py
@@ -1,6 +1,10 @@
 import gensim.downloader as api
 glo100model = api.load("glove-wiki-gigaword-100")
 
+import nltk
+from nltk.tag import pos_tag_sents
+nltk.download('averaged_perceptron_tagger')
+
 # w2v300model = api.load("word2vec-google-news-300")
 # this may be too large for our purposes, but is only english w2v model on gensim
 
@@ -12,11 +16,10 @@ class WordEmbed:
             embeds.append(glo100model[word_tup[0].lower()])
         return embeds
     
-    # currently tokenize_question_and_doc is returning a list of lists (one for each sentence)
     def glove_embed(doc):
         doc_embeds = []
         for sentence in doc:
-            doc_embeds.append(glove_embed_sent(sentence))
+            doc_embeds.append(WordEmbed.glove_embed_sent(sentence))
         return doc_embeds
 
     # def w2v_embed_sent(sentence):
@@ -33,8 +36,25 @@ class WordEmbed:
 
 class FeatExt:
 
-    def pos_tag(doc):
-        pass
+    def untuple(doc):
+        untuped_doc = []
+        for sentence in doc:
+            untuped_sent = []
+            for tup in sentence:
+                untuped_sent.append(tup[0])
+            untuped_doc.append(untuped_sent)
+        return untuped_doc
+    
+    def get_pos_tags(doc):
+        list_of_pos = pos_tag_sents(doc)
+        list_of_pos_tags = []
+        for sentence in list_of_pos:
+            sent_pos = []
+            for tup in sentence:
+                sent_pos.append(tup[1])
+                # need to add step to convert tag into index before appending
+            list_of_pos_tags.append(sent_pos)
+        return list_of_pos_tags
 
     def tf_idf(doc):
         pass
