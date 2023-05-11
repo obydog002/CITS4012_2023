c225a76255a6438be1f032cb6473650928512a34
Max Chatfield <mchatfield@posteo.net>
2023-05-12 06:33:10 +0800

    padding, try to get attention working

--- src/QA/model/QA_RNN.py
+++ src/QA/model/QA_RNN.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.nn.functional as F
+import torch
 
 # QA with document RNN model
 # use attention between the question and document.
@@ -11,12 +12,20 @@ class DocumentModel(nn.Module):
         super(DocumentModel, self).__init__()
 
         self.rnn = nn.RNN(n_input, n_hidden, batch_first=True, bidirectional=True)
+        self.rnn_to_att = nn.Linear(2*n_hidden,n_hidden)
         self.out = nn.Linear(2*n_hidden,n_class)
+
+    def dot(self, t1, t2):
+        batch_size = t1.shape[0]
+        layer_size = t1.shape[1]
+        print(t1.shape)
+        print(t2.shape)
+        return torch.bmm(t1.view(batch_size, 1, layer_size), t2.view(batch_size, layer_size, 1))
 
     def calc_attention(self, hidden, question_hidden, method):
         if method == DocumentModel.ATTN_TYPE_DOT_PRODUCT:
-            weights =  F.softmax(torch.bmm(hidden, question_hidden), dim=-1)
-            attention_output = torch.bmm(weigths, question_hidden)
+            weights =  F.softmax(self.dot(hidden, question_hidden), dim=-1)
+            attention_output = self.dot(weights, question_hidden.unsqueeze(0))
             catted_output = torch.cat((attention_output, hidden), 1)
             
         return catted_output
@@ -24,7 +33,8 @@ class DocumentModel(nn.Module):
     def forward(self, input, question_hidden):        
         _, h_n = self.rnn(input)
         # concat the last hidden states for both directions
-        hidden_out = torch.cat((h_n[-1,:,:],h_n[-2,:,:]),1)
+        rnn_out = torch.cat((h_n[-1,:,:],h_n[-2,:,:]),1)
+        hidden_out = self.rnn_to_att(rnn_out)
 
         catted_output = self.calc_attention(hidden_out, question_hidden, DocumentModel.ATTN_TYPE_DOT_PRODUCT)
 
